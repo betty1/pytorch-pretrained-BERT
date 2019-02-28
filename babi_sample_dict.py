1ec6c23fcf434b26abab93356ac5be82f24b74b4
Betty van Aken <bettyvanaken@gmail.com>
2019-02-28 10:32:58 +0100

    add scripts for plotting layers

---
+++ babi_sample_dict.py
@@ -0,0 +1,27 @@
+samples = {
+    "correct_where_is_the_milk": {
+        "pred_index": range(12, 13),
+        "question_parts": [range(1, 6)],
+        "sp_parts": [range(42, 48), range(66, 72)]
+    },
+    "correct_where_is_sandra": {
+        "pred_index": range(60, 61),
+        "question_parts": [range(1, 5)],
+        "sp_parts": [range(48, 54), range(54, 62)]
+    },
+    "correct_where_is_mary": {
+        "pred_index": range(49, 50),
+        "question_parts": [range(1, 5)],
+        "sp_parts": [range(36, 43), range(43, 51)]
+    },
+    "wrong_where_is_the_milk": {
+        "pred_index": range(33, 34),
+        "question_parts": [range(1, 6)],
+        "sp_parts": [range(42, 48), range(66, 72)]
+    },
+    "wrong_where_is_the_apple": {
+        "pred_index": range(61, 62),
+        "question_parts": [range(1, 6)],
+        "sp_parts": [range(37, 44), range(76, 82), range(82, 87)]
+    }
+}
