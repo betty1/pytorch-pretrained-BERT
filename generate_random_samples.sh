1196804a3ee0675690b844e1dd1c50ba10792696
Benjamin Winter <Benjamin.Winter@beuth-hochschule.de>
2019-03-13 15:21:06 +0100

    Fix output path substitution

--- generate_random_samples.sh
+++ generate_random_samples.sh
@@ -5,15 +5,15 @@ do
   for t in $( seq 1 20 )
   do
     cd examples
-    CUDA_VISIBLE_DEVICES=3 python run_$1.py \
+    CUDA_VISIBLE_DEVICES=0 python run_$1.py \
       --bert_model bert-base-uncased \
       --do_lower_case \
       --output_dir "model_dir" \
       --do_predict \
-      --predict_file "$2/qa$t/test.json" \
+      --predict_file "${2}/qa${t}/test.json" \
       --eval_single_sample \
-      --sample_output_dir "../babi_samples/qa$t_base_$4/qa$t_$i"
+      --sample_output_dir "../babi_samples/base_${4}/qa${t}_${i}"
     cd ../
-    python interpret_layers_babi.py --babi_task "qa$t_base_$4" --sample_name "qa$t_$i"
+    python interpret_layers_babi.py --babi_task "base_${4}" --sample_name "qa${t}_${i}"
   done
 done
